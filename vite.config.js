fabe8980395276ef5182adc2170154b679070443
sonia <soniayjw@gmail.com>
2025-02-22 23:25:19 +0800

    chore: updated vite config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/2024ReactWeek6/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/2024ReactWeek6Submit/" : "/",
   plugins: [react()],
 })
